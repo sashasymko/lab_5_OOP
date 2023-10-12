ab1c303240378a0bfe6a4f66fd0878276e44c661
sashasymko <116544323+sashasymko@users.noreply.github.com>
2023-10-12 17:41:31 +0300

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,21 +1,18 @@
 package flower.store;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 
-@Getter 
+@Getter @Setter
 public class FlowerPack {
     private Flower flower;
     private int quantity;
      
-    public FlowerPack(Flower flower, int quantity) {
+    public FlowerPack(final Flower flower, final int quantity) {
         this.flower = new Flower(flower);
         this.quantity = quantity;
     }
-    public void setQuantity(int quantity) {
-        this.quantity = quantity < 1 ? 1 : quantity;
-    }
-    public double getPrice(){
+    public double getPrice() {
         return flower.getPrice() * quantity;
     }
-    
 }
